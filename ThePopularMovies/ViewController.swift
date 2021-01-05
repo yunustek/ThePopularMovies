9d3615f79f190424299cc5b8f0ec888899153466
Yunus Tek <ynstek@hotmail.com>
2021-01-05 21:20:54 +0300

    initial commit

---
+++ ThePopularMovies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ThePopularMovies
+//
+//  Created by Yunus Tek on 5.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ThePopularMoviesTests/ThePopularMoviesTests.swift
@@ -0,0 +1,33 @@
+//
+//  ThePopularMoviesTests.swift
+//  ThePopularMoviesTests
+//
+//  Created by Yunus Tek on 5.01.2021.
+//
+
+import XCTest
+@testable import ThePopularMovies
+
+class ThePopularMoviesTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
