92d9da00427458c7800f0f86dd8c7291780b4d23
Yunus Tek <ynstek@gmail.com>
2021-01-06 18:35:05 +0300

    Network test created

--- ThePopularMoviesTests/ThePopularMoviesTests.swift
+++ ThePopularMoviesTests/ThePopularMoviesTests.swift
@@ -8,26 +8,41 @@
 import XCTest
 @testable import ThePopularMovies
 
-class ThePopularMoviesTests: XCTestCase {
+class ServiceTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    var provider: Provider?
+
+    override func setUp() {
+        super.setUp()
+        provider = Provider()
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    override func tearDown() {
+        provider = nil
+        super.tearDown()
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+    func test_fetch_popular_movies() {
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+        // Given A Provider
+        let provider = self.provider!
+
+        // When fetch movies
+        let expect = XCTestExpectation(description: "callback")
+
+        provider.fetchMovies(with: .movies(pageNo: 1), successClosure: { movies in
+            expect.fulfill()
+            XCTAssertEqual(movies?.results?.count, 20)
+            for movie in movies?.results ?? [] {
+                XCTAssertNotNil(movie.id)
+            }
+
+        }, errorClosure: { (error) in
+
+        })
+
+        wait(for: [expect], timeout: 0.5)
     }
 
 }
+
